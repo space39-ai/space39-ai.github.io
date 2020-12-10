3c2a24fb147026ba7c66a7983145b29c88d78890
space39-ai <55810399+space39-ai@users.noreply.github.com>
2020-12-10 22:14:49 +0300

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,3 +1,42 @@
 <?php
 
-echo "Random value is " . rand();
+/* Предположим, что это страница может быть доступна по http://example.com/operator.php */
+
+class Operator {
+
+    /**
+     * Складываем два операнда
+     * @param interge 
+     * @return interge
+     */
+    public function add($a, $b) {
+        return $this->_add($a, $b);
+    }
+
+    /**
+     * Вычитаем 
+     */
+    public function sub($a, $b) {
+        return $a - $b;
+    }
+
+    /**
+     * Умножаем
+     */
+    public function mul($a, $b) {
+        return $a * $b;
+    }
+
+    /**
+     * Защищенный метод
+     * @param interge 
+     * @return interge
+     */
+    protected function _add($a, $b) {
+        return $a + $b;
+    }
+}
+
+$server = new Yar_Server(new Operator());
+$server->handle();
+?>
